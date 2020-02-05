bd382e97a1737d689cc188ee59cefb6ba46714f6
clementss <mclements@companieshouse.gov.uk>
2020-02-05 14:51:01 +0000

    add capacity provider

--- ec2.tf
+++ ec2.tf
@@ -102,3 +102,19 @@ resource "aws_autoscaling_group" "ecs-autoscaling-group" {
     }
   ]
 }
+
+resource "aws_ecs_capacity_provider" "test" {
+  name = "example"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.ecs-auto-scaling-group.arn
+    managed_termination_protection = "ENABLED"
+
+    managed_scaling {
+      maximum_scaling_step_size = 1000
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 100
+    }
+  }
+}
--- ecs.tf
+++ ecs.tf
@@ -1,3 +1,7 @@
 resource "aws_ecs_cluster" "ecs-cluster" {
   name = "${var.name_prefix}-cluster"
+  default_capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.test.arn
+    weight = 1
+  }
 }
