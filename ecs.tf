978766b1360b027564ea21704f725ad34c69fcc7
Marc Ransome <mransome@companieshouse.gov.uk>
2023-04-06 12:40:11 +0100

    Encapsulate provider config from dependent projects

--- ecs.tf
+++ ecs.tf
@@ -2,6 +2,6 @@ resource "aws_ecs_cluster" "ecs-cluster" {
   name = "${var.name_prefix}-cluster"
   setting {
     name  = "containerInsights"
-    value = var.container_insights_enablement
+    value = var.enable_container_insights ? "enabled" : "disabled"
   }
 }
